811c0aa57375f0bdfc034b47f310698f523b7ce1
FallThunder <pratyush.siva@gmail.com>
2023-12-30 15:27:34 -0500

    worked on day 4

--- day4/day4.py
+++ day4/day4.py
@@ -30,36 +30,44 @@ def find_matches(current, winners, final):
     
     return final, matches
 
-# day 4 part 1
-with open('day4/day4.txt', 'r') as f:
-    lines = f.read().split('\n')
+# # day 4 part 1
+# with open('day4/day4.txt', 'r') as f:
+#     lines = f.read().split('\n')
 
-card = 1
-final = 0
+# card = 1
+# final = 0
 
-for i in lines:
-    winners = extract_winners(i, card)
-    current = extract_current(i)
-    final = find_matches(current, winners, final)[0]
+# for i in lines:
+#     winners = extract_winners(i, card)
+#     current = extract_current(i)
+#     final = find_matches(current, winners, final)[0]
 
-    card += 1
+#     card += 1
 
-print(final)
+# print(final)
 
 # day 4 part 2
 with open('day4/day4_test.txt', 'r') as f:
     lines = f.read().split('\n')
 
-card_list = [1] * len(lines)
+card_count = {x:1 for x in lines}
+card = 1
 
-for i in lines:
+for i in card_count:
+    card_iter = list(card_count).index(i)
+    final = 0
+
     winners = extract_winners(i, card)
     current = extract_current(i)
-    matches = find_matches(current, winners, final)[1]
+    final = find_matches(current, winners, final)[0]
 
-    for i in range(1, matches + 1):
-        card_list[i] += 1
+    for n in range(1, final + 1):
+        for l in card_count:
+            if list(card_count).index(l) > card_iter:
+                card_count[l] += 1*card_count[i]
 
     card += 1
+    print(card_count)
+    print(sum(card_count[z] for z in card_count), "\n")
 
-print(sum(card_list))
+print(sum(card_count[z] for z in card_count))
